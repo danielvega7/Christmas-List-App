550825ba61a9f090c2ee824a5a9a0f7ec27e1e35
Daniel Vega <drvega7@icloud.com>
2021-10-24 00:31:45 -0500

    Initial Commit

---
+++ Christmas List App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Christmas List App
+//
+//  Created by Daniel Vega on 10/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Christmas List AppTests/Christmas_List_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Christmas_List_AppTests.swift
+//  Christmas List AppTests
+//
+//  Created by Daniel Vega on 10/24/21.
+//
+
+import XCTest
+@testable import Christmas_List_App
+
+class Christmas_List_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
